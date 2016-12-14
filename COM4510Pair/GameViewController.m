63444de6dba1487e547e9a2a70e1df96f1bbaa03
Aaron Claydon <aclaydon1@sheffield.ac.uk>
2016-12-14 16:00:21 +0000

    IT WORKS

--- COM4510Pair/GameViewController.m
+++ COM4510Pair/GameViewController.m
@@ -35,9 +35,9 @@
     self.gameArray = [@[
                          @[ @"red", @"green", @"blue", @"yellow", @"orange", @"red", @"red" ],
                          @[ @"red", @"green", @"blue", @"yellow", @"orange", @"red", @"red" ],
-                         @[ @"red", @"green", @"blue", @"yellow", @"orange", @"red", @"red" ],
+                         @[ @"red", @"green", @"blue", @"yellow", @"orange", @"blue", @"blue" ],
                          @[ @"yellow", @"green", @"blue", @"yellow", @"orange", @"red", @"red" ],
-                         @[ @"red", @"green", @"blue", @"yellow", @"orange", @"red", @"red" ],
+                         @[ @"red", @"green", @"blue", @"blue", @"orange", @"red", @"red" ],
                          @[ @"red", @"green", @"blue", @"yellow", @"orange", @"red", @"red" ],
                          @[ @"red", @"green", @"blue", @"yellow", @"orange", @"red", @"red" ],
                          @[ @"red", @"green", @"blue", @"yellow", @"orange", @"red", @"red" ],
@@ -88,52 +88,66 @@
     NSLog(@"button clicked %@ %i %i %i", tileType, row, column, score);
 }
 
--(int)checkClusterMatchForTile:(NSString*)tile inRow:(int)row andColumn:(int)column {
+-(int)checkClusterMatchForTile:(NSString *)tile inRow:(int)row andColumn:(int)column {
     int score = 0;
+    NSMutableArray* toCheckArray = [[NSMutableArray alloc] init];
     
-    NSLog(@"row %i column %i", row, column);
+    //Add the first point to the stack of tiles to check
+    [self addPointToCheckArray:toCheckArray withRow:row andColumn:column];
     
-    if([[[self.checkedArray objectAtIndex:row] objectAtIndex:column] isEqual: @false]) {
-        //[[self.checkedArray objectAtIndex:row] replaceObjectAtIndex:column withObject:@true];
-        NSMutableArray* rowArray = [self.checkedArray objectAtIndex:row];
-        [rowArray replaceObjectAtIndex:column withObject:@true];
-        [self.checkedArray replaceObjectAtIndex:row withObject:rowArray];
+    while ([toCheckArray count] > 0) {
+        //Get and remove the first value from the stack
+        NSValue* pointValue = [toCheckArray objectAtIndex:0];
+        CGPoint point;
+        [pointValue getValue:&point];
+        [toCheckArray removeObjectAtIndex:0];
         
+        //TODO: CHANGE THIS TO A CUSTOM STRUCT USING INTS NOT FLOATS
+        row = point.x;
+        column = point.y;
+        
+        //Incrase the score by one
+        score++;
+        
+        //Check the nearby touching tiles
         if(row - 1 >= 0) {
             if([[self.gameArray objectAtIndex:row - 1] objectAtIndex:column] == tile) {
-                NSLog(@"row -1 match");
-                score++;
-                
-                score += [self checkClusterMatchForTile:tile inRow:row-1 andColumn:column];
+                [self addPointToCheckArray:toCheckArray withRow:row-1 andColumn:column];
             }
         }
         if(row + 1 < self.height) {
             if([[self.gameArray objectAtIndex:row + 1] objectAtIndex:column] == tile) {
-                NSLog(@"row +1 match");
-                score++;
-                
-                score += [self checkClusterMatchForTile:tile inRow:row+1 andColumn:column];
+                [self addPointToCheckArray:toCheckArray withRow:row+1 andColumn:column];
             }
         }
         if(column - 1 >= 0) {
             if([[self.gameArray objectAtIndex:row] objectAtIndex:column - 1] == tile) {
-                NSLog(@"column -1 match");
-                score++;
-                
-                score += [self checkClusterMatchForTile:tile inRow:row andColumn:column-1];
+                [self addPointToCheckArray:toCheckArray withRow:row andColumn:column-1];
             }
         }
         if(column + 1 < self.width) {
             if([[self.gameArray objectAtIndex:row] objectAtIndex:column + 1] == tile) {
-                NSLog(@"column +1 match");
-                score++;
-                
-                score += [self checkClusterMatchForTile:tile inRow:row andColumn:column+1];
+                [self addPointToCheckArray:toCheckArray withRow:row andColumn:column+1];
             }
         }
+
     }
-    NSLog(@"score %i", score);
+    
     return score;
+}
+
+-(void)addPointToCheckArray:(NSMutableArray*)toCheckArray withRow:(int)row andColumn:(int)column {
+    //Only add points that haven't already been checked
+    if([[[self.checkedArray objectAtIndex:row] objectAtIndex:column] isEqual: @false]) {
+        CGPoint structValue = CGPointMake(row, column);
+        NSValue *value = [NSValue valueWithBytes:&structValue objCType:@encode(CGPoint)];
+        [toCheckArray addObject:value];
+        
+        //Set the point as being checked
+        NSMutableArray* rowArray = [self.checkedArray objectAtIndex:row];
+        [rowArray replaceObjectAtIndex:column withObject:@true];
+        [self.checkedArray replaceObjectAtIndex:row withObject:rowArray];
+    }
 }
 
 /*
